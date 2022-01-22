bed01b327938df44d98adffbb334abbd9dc123c1
kichajster <crimyer@gmail.com>
2022-01-22 18:36:21 +0100

    initial commit

---
+++ wypozyczalnia samochodow/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace wypozyczalnia_samochodow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
